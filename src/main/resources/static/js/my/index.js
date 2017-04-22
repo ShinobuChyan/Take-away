fa4533da627ac28c67ca64712c75e10e2c7f14e9
lfy55 <sqk_55@163.com>
2017-04-22 10:17:53 +0800

    add register interface

--- src/main/resources/static/js/my/index.js
+++ src/main/resources/static/js/my/index.js
@@ -16,6 +16,9 @@ function random_move() {
 };
 random_move();
 
+var canLogin = false;
+var canRegis = false;
+
 // 登陆注册按钮点击事件
 $('#login').click(function() {
     console.log('登陆');
@@ -38,6 +41,75 @@ $('#register').click(function() {
 var namereg = /^[a-z0-9_]{3,15}$/;
 var pwdreg = /^[a-z0-9_]{6,15}$/;
 var tips = $('#error-tips');
+// 注册操作
+// 验证注册用户名是否存在
+$('#registerName').blur(function() {
+    $(this).next('.err-tips').text('');
+    let registerName = $(this).val();
+    if (namereg.test(registerName)) {
+        $.post('register/verify', { userName: registerName }, (res) => {
+            if (res.code === '1') {
+                $(this).next('.err-tips').text('用户名已存在');
+                return false;
+            }
+        });
+    } else {
+        $(this).next('.err-tips').text('用户名为3-15个字母数字下划线的组合');
+        return false;
+    }
+    canRegis = true;
+});
+// 验证密码是否正确
+$('#registerPwd').blur(function() {
+    $(this).next('.err-tips').text('');
+    let registerPwd = $(this).val();
+    if (!pwdreg.test(registerPwd)) {
+        $(this).next('.err-tips').text('密码为6-15个字母数字下划线的组合');
+        return false;
+    }
+});
+// 验证密码是否相同
+$('#registerPwd2').blur(function() {
+    $(this).next('.err-tips').text('');
+    let registerPwd = $('#registerPwd').val();
+    let registerPwd2 = $(this).val();
+    if (registerPwd !== registerPwd2) {
+        $(this).next('.err-tips').text('两次密码不同');
+        return false;
+    }
+});
+// 验证注册
+$('#register-submit').click(function() {
+    var name = $('#registerName').val();
+    var pwd = $('#registerPwd').val();
+    var pwd2 = $('#registerPwd2').val();
+    if (!canRegis) {
+        return false;
+    }
+    if (!namereg.test(name)) {
+        $('#registerName + .err-tips').text('用户名为3-15个字母数字下划线的组合');
+        return false;
+    }
+    if (!pwdreg.test(pwd)) {
+        $('#registerPwd + .err-tips').text('密码应该为6-15个字母数字下划线');
+        return false;
+    }
+    if (pwd2 !== pwd) {
+        $('#registerPwd2 + .err-tips').text('两次密码输入不一致');
+        return false;
+    }
+    $('.err-tips').text('');
+    // do register ...
+    $.post('register', { userName: name, password: pwd }, (res) => {
+        if (res.code === '1') {
+            alert(res.msg);
+            return false;
+        } else if (res.code === '0') {
+            // 登陆成功
+            $('.close').click();
+        }
+    });
+});
 $('#login-submit').click(function() {
     var name = $('#loginName').val();
     var pwd = $('#loginPwd').val();
@@ -54,37 +126,18 @@ $('#login-submit').click(function() {
     // do login ...
 
 });
-$('#register-submit').click(function() {
-    var name = $('#registerName').val();
-    var pwd = $('#registerPwd').val();
-    var pwd2 = $('#registerPwd2').val();
-    if (!namereg.test(name)) {
-        tips.text('用户名应该为3-15个字母数字下划线');
-        tips.show();
-        return false;
-    }
-    if (!pwdreg.test(pwd)) {
-        tips.text('密码应该为6-15个字母数字下划线');
-        tips.show();
-        return false;
-    }
-    if (pwd2 !== pwd) {
-        tips.text('两次密码输入不一致');
-        tips.show();
-        return false;
-    }
-    // do register ...
-});
+
 
 $('input').focus(function() {
     tips.hide();
+    tips.text('');
 });
 
 
 
 $('.close').click(function() {
     $(this).parent('.dlg').animateCss('fadeOut');
-    setTimeout(function () {
+    setTimeout(() => {
         $(this).parent('.dlg').removeClass('show');
     }, 740);
 });
